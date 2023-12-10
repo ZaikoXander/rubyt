fb741fe0ee36be6dc90bc17dfb08709ae6ecb9a0
ZaikoXander <80606136+ZaikoXander@users.noreply.github.com>
2023-12-10 17:48:57 -0300

    test(RubytMustNotBeNilError): Improve tests names, structure and logic

--- spec/lib/types/errors/rubyt_must_not_be_nil_error_spec.rb
+++ spec/lib/types/errors/rubyt_must_not_be_nil_error_spec.rb
@@ -4,20 +4,20 @@ require_relative '../../../../lib/types/errors/rubyt_must_not_be_nil_error'
 
 describe RubytMustNotBeNilError do
   describe '#initialize' do
-    it 'is a RubytError' do
+    it 'instantiates' do
       rubyt_must_not_be_nil_error = RubytMustNotBeNilError.new
       expect(rubyt_must_not_be_nil_error).to be_a RubytMustNotBeNilError
       expect(rubyt_must_not_be_nil_error).to be_a RubytError
     end
+  end
 
-    it 'shows a message' do
-      rubyt_must_not_be_nil_error = RubytMustNotBeNilError.new
-      expect(rubyt_must_not_be_nil_error.message).to eq 'must not be nil'
+  describe 'raising' do
+    it 'raises with a message' do
+      expect { raise RubytMustNotBeNilError }.to raise_error RubytMustNotBeNilError, 'must not be nil'
     end
 
-    it 'shows a custom message' do
-      rubyt_must_not_be_nil_error = RubytMustNotBeNilError.new('Custom message')
-      expect(rubyt_must_not_be_nil_error.message).to eq 'Custom message'
+    it 'raises with a custom message' do
+      expect { raise RubytMustNotBeNilError, 'Custom message' }.to raise_error RubytMustNotBeNilError, 'Custom message'
     end
   end
 end
