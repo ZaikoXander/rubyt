187dba2fda838a92d85f91ced47318c0f5d33bcb
ZaikoXander <80606136+ZaikoXander@users.noreply.github.com>
2023-12-15 20:00:40 -0300

    feat: Add new types to rubyt.rb imports

--- lib/rubyt.rb
+++ lib/rubyt.rb
@@ -4,7 +4,10 @@
 
 require_relative 'types/monkey_patching/object'
 require_relative 'types/any'
+require_relative 'types/array_t'
 require_relative 'types/boolean'
+require_relative 'types/float_t'
+require_relative 'types/hash_t'
 require_relative 'types/integer_t'
 require_relative 'types/nilable_string'
 require_relative 'types/string_t'
