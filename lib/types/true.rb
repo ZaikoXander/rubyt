48671a3fc0b3d78cc4e5a65648c720c10b1c9998
ZaikoXander <80606136+ZaikoXander@users.noreply.github.com>
2023-12-15 22:19:15 -0300

    feat(True): Create true type

---
+++ lib/types/true.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+require_relative 'any'
+require_relative 'errors/rubyt_type_error'
+
+class True < Any
+  class << self
+    def t(value)
+      raise RubytTypeError.new('True', value.class) unless value.is_a? True
+
+      value
+    end
+  end
+
+  private
+
+  def valid_value?(value)
+    value.is_a? TrueClass
+  end
+
+  def validate_value(value)
+    raise RubytTypeError.new('TrueClass', value.class) unless valid_value? value
+  end
+
+  def setup_value(value)
+    if value.is_a? True
+      validate_value(value.t)
+
+      return value.t
+    end
+
+    validate_value(value)
+
+    value
+  end
+
+  public
+
+  def initialize(value)
+    super(setup_value(value))
+  end
+end
